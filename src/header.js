435ca593f696163bf6e231ac0dd6f2a61c0cd2d2
Doug McDonald <dougajmcdonald@gmail.com>
2020-07-17 19:13:12 +0100

    Add tabs and old manifest icons, favicons etc

--- src/header.js
+++ src/header.js
@@ -1,5 +1,10 @@
 import React from 'react'
+import { ReactComponent as Logo } from './img/logo.svg'
 
-const Header = () => <header className="w-full h-16"></header>
+const Header = () => (
+  <header className="w-full h-16">
+    <Logo title="corewar logo" />
+  </header>
+)
 
 export default Header
--- src/tab.js
+++ src/tab.js
@@ -4,7 +4,7 @@ const Tab = ({ children, active }) => {
   const activeClasses = 'bg-gray-800'
   return (
     <button
-      className={`w-20 h-10 border border-b-0 border-gray-700 rounded-lg rounded-b-none text-xs font-semibold text-gray-100 ${
+      className={`w-32 h-10 border border-b-0 border-gray-700 rounded-lg rounded-b-none text-xs font-semibold text-gray-100 ${
         active && activeClasses
       }`}
     >
