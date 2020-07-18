8a6291bfec7feea8892209c8c8d68dd6916bfb09
Doug McDonald <dougajmcdonald@gmail.com>
2020-07-18 16:02:41 +0100

    tweak a few bits

--- src/features/files/code-editor.js
+++ src/features/files/code-editor.js
@@ -1,17 +1,21 @@
 import React from 'react'
 
 const CodeEditor = () => (
-  <section className="flex w-full p-2 rounded-lg rounded-tl-none bg-gray-700 text-gray-100 text-sm">
+  <section className="flex w-full p-2 rounded-lg rounded-tl-none bg-gray-700 text-gray-100">
     <textarea
-      className="w-full h-full bg-transparent font-code resize-none overflow-auto whitespace-pre-wrap"
+      className="w-full h-full bg-transparent font-code resize-none overflow-auto whitespace-pre-wrap mx-4"
       auto-complete="off"
       auto-correct="off"
       auto-capitalize="off"
       spellCheck="false"
-    >
-      ;name Transposition Stone step equ 1185 ; mod 5 inc spl #-step, &lt;step stone mov &gt;step,
-      1-step sub inc, stone djn.f stone, &lt;5555
-    </textarea>
+      defaultValue={`;name Transposition Stone 
+step equ 1185; 
+mod 5 inc spl #-step, 
+<step stone mov >step,
+1-step sub inc, 
+stone djn.f stone, 
+<5555`}
+    ></textarea>
   </section>
 )
 
--- src/features/simulator/wrapper.js
+++ src/features/simulator/wrapper.js
@@ -1,11 +1,40 @@
 import React from 'react'
+import { ReactComponent as PlayIcon } from '../../img/icons/play-circle-outline.svg'
 
 const Wrapper = () => (
-  <section className="w-3/5 flex flex-row items-stretch">
-    <section className="w-1/3">Core inspection</section>
-    <section className="w-2/3">
-      <div className="w-full h-40">top bit</div>
-      <div className="bg-gray-500 rounded w-128 h-128">core</div>
+  <section className="w-3/5 flex flex-row p-2">
+    <section className="mt-40 flex-1 p-8">
+      <table className="text-sm w-full text-right">
+        <thead className="font-semibold h-12">
+          <td>Address</td>
+          <td>Instruction</td>
+          <td>A Operand</td>
+          <td>B Operand</td>
+        </thead>
+        <tr className="font-code h-8">
+          <td>386</td>
+          <td>MOV.I</td>
+          <td>&gt;1185</td>
+          <td>$-1184</td>
+        </tr>
+        <tr className="font-code h-8">
+          <td>386</td>
+          <td>MOV.I</td>
+          <td>&gt;1185</td>
+          <td>$-1184</td>
+        </tr>
+      </table>
+    </section>
+    <section className="w-core flex flex-col items-end">
+      <div className="w-full h-30 flex-col pt-2">
+        <div className="w-full h-8 bg-gray-700 rounded flex items-center justify-center">100%</div>
+        <div className="w-2/5 h-6 mt-2 bg-red-700 rounded"></div>
+        <div className="w-3/5 h-6 mt-2 bg-blue-700 rounded"></div>
+      </div>
+      <div className="h-16 w-full flex justify-start items-center text-gray-100">
+        <PlayIcon className="stroke-current h-12 w-12"></PlayIcon>
+      </div>
+      <div className="bg-gray-500 rounded w-core h-core">core</div>
     </section>
   </section>
 )
